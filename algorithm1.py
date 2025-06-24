cfd4488c7379f9687cfd51fdae6923c35f167afa
ApaarRaina <apaarraina@gmail.com>
2025-06-24 17:28:17 +0530

    updated code

--- algorithm1.py
+++ algorithm1.py
@@ -20,8 +20,8 @@ def Iris_localisation(img):
         img_w,img_h=img.shape[1],img.shape[0]
         radii=np.linspace(0.1*img_w,0.5*img_w,400)
 
-        center_x=np.random.randint(img_w/8,7*img_w/8,200)
-        center_y=np.random.randint(img_h/8,7*img_h/8,200)
+        center_x=np.random.randint(img_w//8,7*img_w//8,200)
+        center_y=np.random.randint(img_h//8,7*img_h//8,200)
 
         h,w=img.shape[0],img.shape[1]
         coord_x=np.indices((h,w))[1]
@@ -78,8 +78,7 @@ def Iris_localisation(img):
 
 
 
-def iris_normalisation(r_pupil,r_iris,img):
-        r_pupil,r_iris=min([r_best,r_second]),max([r_best,r_second])
+def iris_normalisation(img,r_pupil,r_iris,x_center,y_center):
         radii=np.linspace(0,1,360)
         theta = np.linspace(0, 2 * np.pi, 720)
 
@@ -100,6 +99,50 @@ def iris_normalisation(r_pupil,r_iris,img):
 
         return processed_img
 
+
+def roi_crop(processed_img,r_pupil,r_iris,x_center,y_center):
+        theta=np.radians([30,-30])
+
+        x_p=x_center + r_pupil * np.cos(theta)
+        y_p=y_center + r_pupil * np.sin(theta)
+
+        x_i=x_center + r_iris * np.cos(theta)
+        y_i=y_center + r_iris * np.sin(theta)
+
+        x_p = np.round(x_p).astype(int)
+        y_p = np.round(y_p).astype(int)
+        x_i = np.round(x_i).astype(int)
+        y_i = np.round(y_i).astype(int)
+
+        c_1=[x_p[0],y_p[0]]
+        c_2=[x_i[0],y_i[0]]
+        c_3=[x_i[1],y_i[1]]
+        c_4=[x_p[1],y_p[1]]
+
+        pts = np.array([c_1,c_2,c_3,c_4])
+
+        mask = np.zeros_like(processed_img, dtype=np.uint8)
+
+        cv.fillPoly(mask, [pts], 255)
+
+        # Bitwise AND to extract the region
+        roi = cv.bitwise_and(processed_img, processed_img, mask=mask)
+
+        # Get the tight bounding box of the polygon
+        x, y, w, h = cv.boundingRect(pts)
+
+        roi = roi[y:y+h, x:x+w]
+
+        return roi
+
+def roi_enhancement(roi):
+        roi = roi.astype('uint8')
+
+        clahe = cv.createCLAHE(clipLimit=4.0, tileGridSize=(8, 8))
+
+        roi_equalised = clahe.apply(roi)
+
+        return roi_equalised
 
 
 
